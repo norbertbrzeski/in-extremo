0601a142919899da4747a1f9a8eb73fe095da640
norbert <norbert-brzeski@wp.pl>
2018-01-17 13:39:52 +0100

    create basic template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    NavLink
+} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
+import './App.css'
 
-export default App;
+
+const BasicExample = () => (
+    <Router>
+        <div  id="container">
+            <div id="main-header">in-extremo</div>
+
+            <div id="body">
+            <ul className="header" >
+                <li><NavLink exact to="/" activeClassName="active" >in-extremo</NavLink></li>
+                <li><NavLink to="/Stuff" activeClassName="active">Topics  </NavLink></li>
+                <li><NavLink  to="/Contact" activeClassName="active">Topics</NavLink></li>
+            </ul>
+
+
+
+                <hr/>
+            <div className="content">
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/Stuff" component={Stuff}/>
+            <Route path="/Contact" component={Contact}/>
+            </div>
+            </div>
+
+
+
+        </div>
+    </Router>
+);
+
+
+export default BasicExample
