9283f8fbd48216ced811cd49005a446cfbcd2736
norbert <norbert-brzeski@wp.pl>
2018-01-21 07:00:06 +0100

    ready to review @robert-jemiolo

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,53 @@ import React, { Component } from "react";
 
 import { Parallax } from 'react-parallax';
 import './App.css'
+import Mail from 'react-icons/lib/md/mail'
+import Face from 'react-icons/lib/md/face'
+import Box from 'react-icons/lib/md/account-box'
+import Scanner from 'react-icons/lib/md/scanner'
+
 class Home extends Component {
     render() {
         return (
             <div className=' animated  fadeIn '>
 
-                <div className='navbar'>
-                <div id="section-1" className='HomeDivs'>Cras facilisis urna ornare ex volutpat, et
-                    convallis erat elementum. Ut aliquam, ipsum vitae
-                    gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                    metus nec massa. Maecenas hendrerit laoreet augue
-                    nec molestie. Cum sociis natoque penatibus et magnis
-                    dis parturient montes, nascetur ridiculus mus.</div>
+
+                <div id="section-1" className='HomeDivs'> <div className="col-lg-8 my-auto">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <div className="feature-item">
+                                    <Mail />
+                                    <h3>Device Mockups</h3>
+                                    <p className="text-muted">Ready to use HTML/CSS device mockups, no Photoshop required!</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="feature-item">
+                                    <Face/>
+                                    <h3>Flexible Use</h3>
+                                    <p className="text-muted">Put an image, video, animation, or anything else in the screen!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <div className="feature-item">
+                                        <Scanner/>
+                                    <h3>Free to Use</h3>
+                                    <p className="text-muted">As always, this theme is free to download and use for any purpose!</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="feature-item">
+                                            <Box/>
+                                    <h3>Open Source</h3>
+                                    <p className="text-muted">Since this theme is MIT licensed, you can use it commercially!</p>
+                                </div>
+                            </div>
+                        </div>
+
+                </div></div>
                 <Parallax
                     blur={{ min: -23, max: 15 }}
                     bgImage={require('./images/bg3.jpg')}
@@ -57,7 +92,6 @@ class Home extends Component {
 
 
 </div>
-
             </div>
         );
     }
